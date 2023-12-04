17971e078d0ad2df6f53a887a10335c2701b23fb
pop-punk <harrison@gaslite.gg>
2023-12-04 14:33:35 -0500

    feat: Gaslite Vest

---
+++ src/GasliteVest.sol
@@ -0,0 +1,147 @@
+pragma solidity 0.8.19;
+
+import "@solady/auth/Ownable.sol";
+import "@solady/tokens/ERC20.sol";
+import {console} from "forge-std/Console.sol";
+
+// forgefmt: disable-start
+/**
+ *                                                           bbbbbbbb                                         dddddddd
+ *                                                           b::::::b                                         d::::::d
+ *                                                           b::::::b                                         d::::::d
+ *                                                           b::::::b                                         d::::::d
+ *                                                            b:::::b                                         d:::::d
+ *    ggggggggg   ggggg aaaaaaaaaaaaa      ssssssssss         b:::::bbbbbbbbb      aaaaaaaaaaaaa      ddddddddd:::::d
+ *   g:::::::::ggg::::g a::::::::::::a   ss::::::::::s        b::::::::::::::bb    a::::::::::::a   dd::::::::::::::d
+ *  g:::::::::::::::::g aaaaaaaaa:::::ass:::::::::::::s       b::::::::::::::::b   aaaaaaaaa:::::a d::::::::::::::::d
+ * g::::::ggggg::::::gg          a::::as::::::ssss:::::s      b:::::bbbbb:::::::b           a::::ad:::::::ddddd:::::d
+ * g:::::g     g:::::g    aaaaaaa:::::a s:::::s  ssssss       b:::::b    b::::::b    aaaaaaa:::::ad::::::d    d:::::d
+ * g:::::g     g:::::g  aa::::::::::::a   s::::::s            b:::::b     b:::::b  aa::::::::::::ad:::::d     d:::::d
+ * g:::::g     g:::::g a::::aaaa::::::a      s::::::s         b:::::b     b:::::b a::::aaaa::::::ad:::::d     d:::::d
+ * g::::::g    g:::::ga::::a    a:::::assssss   s:::::s       b:::::b     b:::::ba::::a    a:::::ad:::::d     d:::::d
+ * g:::::::ggggg:::::ga::::a    a:::::as:::::ssss::::::s      b:::::bbbbbb::::::ba::::a    a:::::ad::::::ddddd::::::dd
+ *  g::::::::::::::::ga:::::aaaa::::::as::::::::::::::s       b::::::::::::::::b a:::::aaaa::::::a d:::::::::::::::::d
+ *   gg::::::::::::::g a::::::::::aa:::as:::::::::::ss        b:::::::::::::::b   a::::::::::aa:::a d:::::::::ddd::::d
+ *     gggggggg::::::g  aaaaaaaaaa  aaaa sssssssssss          bbbbbbbbbbbbbbbb     aaaaaaaaaa  aaaa  ddddddddd   ddddd
+ *             g:::::g
+ * gggggg      g:::::g
+ * g:::::gg   gg:::::g
+ *  g::::::ggg:::::::g
+ *   gg:::::::::::::g
+ *     ggg::::::ggg
+ *        gggggg
+ */
+// forgefmt: disable-end
+
+/// @title GasliteVest
+/// @notice Turbo gas optimized token vesting
+/// @author Harrison (@PopPunkOnChain)
+/// @author Gaslite (@GasliteGG)
+
+contract GasliteVest is Ownable {
+    uint256 public vestingId;
+
+    struct Vesting {
+        address token;
+        address recipient;
+        uint256 amount;
+        uint32 start;
+        uint32 end;
+        uint32 lastClaim;
+        uint256 claimed;
+    }
+
+    mapping(uint256 => Vesting) public vestings;
+
+    error InvalidAddress();
+    error InvalidAmount();
+    error InvalidTimestamp();
+
+    event Created(
+        address indexed token, address indexed recipient, uint256 id, uint256 amount, uint32 start, uint32 end
+    );
+
+    event Cancelled(address indexed token, address indexed recipient, uint256 id, uint256 amount);
+
+    constructor() {
+        _initializeOwner(msg.sender);
+    }
+
+    function create(address token, address recipient, uint256 amount, uint32 start, uint32 end)
+        external
+        onlyOwner
+        returns (uint256)
+    {
+        if (token == address(0)) revert InvalidAddress();
+        if (recipient == address(0)) revert InvalidAddress();
+        if (amount == 0) revert InvalidAmount();
+        if (start < block.timestamp) revert InvalidTimestamp();
+        if (end <= start) revert InvalidTimestamp();
+
+        ERC20(token).transferFrom(msg.sender, address(this), amount);
+
+        uint256 id = vestingId;
+
+        Vesting storage vesting = vestings[id];
+        vesting.token = token;
+        vesting.recipient = recipient;
+        vesting.amount = amount;
+        vesting.start = start;
+        vesting.end = end;
+        vesting.lastClaim = start;
+
+        unchecked {
+            vestingId++;
+        }
+
+        emit Created(token, recipient, id, amount, start, end);
+
+        return id;
+    }
+
+    function claim(uint256 id) external {
+        Vesting storage vesting = vestings[id];
+
+        uint256 amount = vestedAmount(id);
+
+        if (amount == 0) return;
+
+        vesting.lastClaim = uint32(block.timestamp);
+
+        unchecked {
+            vesting.claimed += amount;
+        }
+
+        ERC20(vesting.token).transfer(vesting.recipient, amount);
+    }
+
+    function cancel(uint256 id) external onlyOwner {
+        Vesting storage vesting = vestings[id];
+
+        uint256 vested = vestedAmount(id);
+
+        ERC20(vesting.token).transfer(owner(), vesting.amount - vested);
+        if (vested > 0) {
+            ERC20(vesting.token).transfer(vesting.recipient, vested);
+        }
+
+        emit Cancelled(vesting.token, vesting.recipient, id, vesting.amount);
+        delete vestings[id];
+    }
+
+    function vestedAmount(uint256 id) public view returns (uint256) {
+        Vesting memory vesting = vestings[id];
+
+        if (block.timestamp < vesting.start) return 0;
+        if (block.timestamp >= vesting.end) return vesting.amount - vesting.claimed;
+
+        uint256 timeSinceLastClaim = block.timestamp - vesting.lastClaim;
+        uint256 vestingPeriod = vesting.end - vesting.start;
+
+        return (vesting.amount * timeSinceLastClaim) / vestingPeriod;
+    }
+
+    function getVesting(uint256 id) external view returns (Vesting memory) {
+        return vestings[id];
+    }
+}
---
+++ test/GasliteVest.t.sol
@@ -0,0 +1,153 @@
+pragma solidity 0.8.19;
+
+import {GasliteVest} from "./../src/GasliteVest.sol";
+import {NFT} from "./../test/utils/NFT.sol";
+import {Token} from "./../test/utils/Token.sol";
+import "forge-std/Test.sol";
+
+contract GasliteVestTest is Test {
+    GasliteVest vest;
+    NFT nft;
+    Token token;
+
+    address admin = vm.addr(0x1);
+    address recipient = vm.addr(0x2);
+
+    function setUp() external {
+        vm.startPrank(admin);
+        vest = new GasliteVest();
+        vm.stopPrank();
+        token = new Token();
+        token.transfer(admin, 100e18);
+    }
+
+    function test_create() external {
+        vm.startPrank(admin);
+        token.approve(address(vest), 100e18);
+
+        uint256 id = vest.create(address(token), recipient, 100e18, 10, 20);
+
+        assertEq(id, 0);
+        assertEq(token.balanceOf(address(vest)), 100e18);
+
+        (
+            address _token,
+            address _recipient,
+            uint256 _amount,
+            uint32 _start,
+            uint32 _end,
+            uint32 _lastClaim,
+            uint256 claimed
+        ) = vest.vestings(id);
+        assertEq(_token, address(token));
+        assertEq(_recipient, recipient);
+        assertEq(_amount, 100e18);
+        assertEq(_start, 10);
+        assertEq(_end, 20);
+        assertEq(_lastClaim, 10);
+        assertEq(claimed, 0);
+    }
+
+    function test_claim() external {
+        vm.startPrank(admin);
+        token.approve(address(vest), 100e18);
+
+        uint256 id = vest.create(address(token), recipient, 100e18, 10, 20);
+
+        assertEq(token.balanceOf(address(vest)), 100e18);
+
+        vm.warp(15);
+
+        vest.claim(0);
+
+        assertEq(token.balanceOf(address(vest)), 50e18);
+        assertEq(token.balanceOf(recipient), 50e18);
+
+        (,,,,,, uint256 claimed) = vest.vestings(id);
+        assertEq(claimed, 50e18);
+    }
+
+    function test_claimTwice() external {
+        vm.startPrank(admin);
+        token.approve(address(vest), 100e18);
+
+        uint256 id = vest.create(address(token), recipient, 100e18, 10, 20);
+
+        assertEq(token.balanceOf(address(vest)), 100e18);
+
+        vm.warp(15);
+
+        vest.claim(id);
+
+        assertEq(token.balanceOf(address(vest)), 50e18);
+        assertEq(token.balanceOf(recipient), 50e18);
+
+        (,,,,,, uint256 claimed) = vest.vestings(id);
+        assertEq(claimed, 50e18);
+
+        vm.warp(16);
+
+        vest.claim(id);
+
+        assertEq(token.balanceOf(address(vest)), 40e18);
+        assertEq(token.balanceOf(recipient), 60e18);
+
+        (,,,,,, claimed) = vest.vestings(id);
+        assertEq(claimed, 60e18);
+    }
+
+    function test_claimFull() external {
+        vm.startPrank(admin);
+        token.approve(address(vest), 100e18);
+
+        uint256 id = vest.create(address(token), recipient, 100e18, 10, 20);
+
+        assertEq(token.balanceOf(address(vest)), 100e18);
+
+        vm.warp(20);
+
+        vest.claim(id);
+
+        assertEq(token.balanceOf(address(vest)), 0);
+        assertEq(token.balanceOf(recipient), 100e18);
+
+        (,,,,,, uint256 claimed) = vest.vestings(id);
+        assertEq(claimed, 100e18);
+    }
+
+    function test_cancel() external {
+        vm.startPrank(admin);
+        token.approve(address(vest), 100e18);
+
+        uint256 id = vest.create(address(token), recipient, 100e18, 10, 20);
+
+        assertEq(token.balanceOf(address(vest)), 100e18);
+
+        vest.cancel(id);
+
+        assertEq(token.balanceOf(address(vest)), 0);
+        assertEq(token.balanceOf(recipient),0);
+        assertEq(token.balanceOf(admin), 100e18);
+    }
+
+    function test_cancelVested() external {
+        vm.startPrank(admin);
+        token.approve(address(vest), 100e18);
+
+        uint256 balanceBefore = token.balanceOf(address(this));
+
+        uint256 id = vest.create(address(token), recipient, 100e18, 10, 20);
+
+        assertEq(token.balanceOf(address(vest)), 100e18);
+
+        vm.warp(15);
+
+        vest.cancel(id);
+
+        uint256 balanceAfter = token.balanceOf(address(this));
+
+        assertEq(token.balanceOf(address(vest)), 0);
+        assertEq(token.balanceOf(recipient), 50e18);
+        assertEq(token.balanceOf(admin), 50e18);
+    }
+}
